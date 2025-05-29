5c1815505ff34b9825823f483f2f7543a4cbcbd7
Sandeep Chaudhary <sachaudhary91@gmail.com>
2025-05-29 11:05:02 +0000

    added ts ignore

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -60,7 +60,9 @@ export function AnimatedSection<C extends keyof JSX.IntrinsicElements = 'section
   }, [delay]); // Add delay to the dependency array
   
   return (
+    // @ts-ignore
     <ComponentToRender
+      // @ts-ignore
       ref={sectionRef} // The ref is now correctly typed for ComponentToRender
       id={id}
       className={cn(

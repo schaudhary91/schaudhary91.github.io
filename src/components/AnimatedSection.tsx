8b85651e6e4e7d377425a42a3cf769771cedab3f
Sandeep Chaudhary <schaudhary_91@hotmail.com>
2025-05-29 16:50:54 +0530

    Update AnimatedSection.tsx

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -60,8 +60,8 @@ export function AnimatedSection<C extends keyof JSX.IntrinsicElements = 'section
   }, [delay]); // Add delay to the dependency array
   
   return (
-    // @ts-expect-error -- Component working correctly
     <ComponentToRender
+      // @ts-expect-error -- Component working correctly
       ref={sectionRef} // The ref is now correctly typed for ComponentToRender
       id={id}
       className={cn(
